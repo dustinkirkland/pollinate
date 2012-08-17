34db586ebbd6baaff9f0597fc85d7658a0511e25
Dustin Kirkland <dustin.kirkland@gmail.com>
2012-08-17 01:47:54 -0500

    * anerd.c:   - use syslog, open files/sockets only once per fork

--- anerd.c
+++ anerd.c
@@ -29,6 +29,7 @@ limitations under the License.
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -59,6 +60,11 @@ int anerd_server(char *device, int size, int port) {
 		perror("Socket");
 		exit(1);
 	}
+	/* Open the random device */
+	if ((fp = fopen(device, "a+")) == NULL) {
+		perror("fopen");
+		exit(1);
+	}
 	/* Allocate and zero a data buffer to the chosen size */
 	if ((data = calloc(size, sizeof(char))) == NULL) {
 		perror("calloc");
@@ -87,26 +93,22 @@ int anerd_server(char *device, int size, int port) {
 		salt = last_usec * this_usec;
 		last_usec = this_usec;
 		/* Logging/debug message */
-		printf("Received [%d] bytes from [%s:%d]\n", bytes_read, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		syslog(LOG_INFO, "Received [%d] bytes from [%s:%d]\n", bytes_read, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 		fflush(stdout);
 		/* Mix incoming entropy + salt into pool */
-		if ((fp = fopen(device, "a+")) == NULL) {
-			perror("fopen");
-			/* Error reading/writing entropy; skip this round */
-			continue;
-		}
 		fwrite(data, sizeof(char), bytes_read, fp);
 		fwrite(&salt, sizeof(double), 1, fp);
+		fflush(fp);
 		/* Obtain some entropy for transmission */
 		fread(data, sizeof(char), bytes_read, fp);
-		fclose(fp);
 		/* Return the favor, sending entropy back to the initiator */
 		sendto(sock, data, strlen(data), 0, (struct sockaddr *)&client_addr, sizeof(struct sockaddr));
-		printf("Transmit [%d] bytes to [%s:%d]\n", bytes_read, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		syslog(LOG_INFO, "Transmit [%d] bytes to [%s:%d]\n", bytes_read, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 	}
 	/* Should never get here; clean up if we do */
 	free(data);
 	close(sock);
+	fclose(fp);
 	return 0;
 }
 
@@ -119,52 +121,48 @@ anerd client:
 int anerd_client(char *device, int size, int port, int interval) {
 	int sock;
 	struct sockaddr_in server_addr;
-	struct hostent *host;
 	char *data;
 	FILE *fp;
 	int broadcast = 1;
-	/* Set up the broadcast address */
-	if ((host = gethostbyname("255.255.255.255")) == NULL) {
-		perror("gethostbyname");
-		exit(1);
-	}
 	/* Allocate and zero a data buffer to the chosen size */
 	if ((data = calloc(size, sizeof(char))) == NULL) {
 		perror("calloc");
 		exit(1);
 	}
+	/* Setup the UDP socket */
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		perror("socket");
+	}
+	/* Configure the socket for broadcast */
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast) == -1) {
+		perror("setsockopt (SO_BROADCAST)");
+	}
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+	bzero(&(server_addr.sin_zero),8);
+	if ((fp = fopen(device, "r")) == NULL) {
+		/* Error reading entropy; skip this round */
+		perror("fopen");
+		close(sock);
+		exit(1);
+	}
 	/* Periodically trigger a network entropy exchange */
 	while (interval > 0) {
-		/* Setup the UDP socket */
-		if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-			perror("socket");
+		/* Donate some entropy to the local networks */
+		if (fread(data, 1, size, fp) > 0) {
+			syslog(LOG_INFO, "Donated  [%d] bytes to [%s:%d]\n", size, inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+			sendto(sock, data, size, 0, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+		} else {
+			perror("fread");
 		}
-		/* Configure the socket for broadcast */
-		if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast) == -1) {
-			perror("setsockopt (SO_BROADCAST)");
-		}
-		server_addr.sin_family = AF_INET;
-		server_addr.sin_port = htons(port);
-		server_addr.sin_addr = *((struct in_addr *)host->h_addr);
-		bzero(&(server_addr.sin_zero),8);
-		if ((fp = fopen(device, "r")) == NULL) {
-			perror("fopen");
-			close(sock);
-			/* Error reading entropy; skip this round */
-			continue;
-		}
-		fread(data, 1, size, fp);
-		fclose(fp);
-		sendto(sock, data, size, 0, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-		/* Donate some entropy to the local network pool,
-		   and hopefully trigger some responses */
-		printf("Donating [%d] bytes to [%s:%d]\n", size, inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 		/* Clean-up */
-		close(sock);
 		sleep(interval);
 	}
 	/* Should never get here; clean up if we do */
+	close(sock);
 	free(data);
+	fclose(fp);
 	return 0;
 }
 
@@ -186,6 +184,8 @@ int main(int argc, char *argv[]) {
 			size = atoi(argv[++i]);
 		}
 	}
+	/* Set up syslog */
+	openlog ("anerd", LOG_PERROR | LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON);
 	if (fork() == 0) {
 		/* Fork a client process */
 		anerd_client(device, size, port, interval);
