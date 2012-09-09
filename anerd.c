53b11b05560d81aca006ec87b814bb3fb095398b
Dustin Kirkland <dustin.kirkland@gmail.com>
2012-09-09 18:30:38 -0500

    alphabetize includes

--- anerd.c
+++ anerd.c
@@ -18,20 +18,19 @@ limitations under the License.
 
 */
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <errno.h>
+#include <netdb.h>
 #include <poll.h>
 #include <pwd.h>
-#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
