b5e9b2889d3b2c0deddeefb9937dc9c94375f4c8
Dustin Kirkland <kirkland@ubuntu.com>
2014-01-09 10:38:56 -0600

    * debian/pollinate.cron.d:   - add notes in the comments about NIST DRBG Special Publication 800-90A     recommendations on reseeding   - add notes in the comments about why we choose a random minute   - fix a bug, that was causing the cronjob to run far more frequently     than desired   - Addresses some issues raised by Seth Arnold in LP: #1246098

--- debian/pollinate.cron.d
+++ debian/pollinate.cron.d
@@ -1,2 +1,9 @@
 # Reseed the PRNG regularly, per FIPS 140-2 recommendation
-*/__RAND__ * * * *	daemon	run-one pollinate
+# This was inspired by the NIST DRBG Special Publication 800-90A, which
+# recommends periodically reseeding pseudo random number generators.
+# http://csrc.nist.gov/groups/STM/cavp/documents/drbg/DRBGVS.pdf‎
+
+# At package installation, the postinst will choose a random minute,
+# 0-59, for this pollinate client to run.  The goal here is to spread
+# the load on the entropy server.
+__RAND__ * * * *	daemon	run-one pollinate
