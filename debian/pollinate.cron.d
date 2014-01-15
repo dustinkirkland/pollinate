52a155edd8079c981f04fccbf466acd50dc4dba0
Dustin Kirkland <kirkland@ubuntu.com>
2014-01-15 16:48:21 -0600

    * debian/pollinate.cron.d, debian/pollinate.postinst,   debian/pollinate.postrm:   - run the pollinate cronjob (reseed) once per day, rather than once     per hour   - purge pollinate files more effectively

--- debian/pollinate.cron.d
+++ debian/pollinate.cron.d
@@ -3,7 +3,7 @@
 # recommends periodically reseeding pseudo random number generators.
 # http://csrc.nist.gov/groups/STM/cavp/documents/drbg/DRBGVS.pdf‎
 
-# At package installation, the postinst will choose a random minute,
-# 0-59, for this pollinate client to run.  The goal here is to spread
-# the load on the entropy server.
-__RAND__ * * * *	daemon	run-one pollinate
+# At package installation, the postinst will choose a random
+# hour:minute, for this pollinate client to run each day.
+# The goal here is to spread the load on the entropy server.
+__RAND_MINUTE__ __RAND_HOUR__ * * *	daemon	run-one pollinate
